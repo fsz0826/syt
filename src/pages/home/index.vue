211c6dda5f7d44cd0246b275efc08a141415e9f2
fsz0826 <513302151@qq.com>
2025-10-23 14:16:23 +0800

    制作搜索栏

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <Carousel />
+    <Search />
   </div>
 </template>
 <script lang="ts" setup>
   import Carousel from "./carousel/index.vue";
+  import Search from "./search/index.vue";
 </script>
 <style lang="scss" scoped></style>
---
+++ src/pages/home/search/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="search">
+    <el-autocomplete clearable placeholder="请输入医院名称" />
+    <el-button type="primary" size="default" :icon="Search">搜索</el-button>
+  </div>
+</template>
+<script lang="ts" setup>
+  import { Search } from "@element-plus/icons-vue";
+</script>
+<style lang="scss" scoped>
+  .search {
+    width: 100%;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px 0;
+    :deep(.el-autocomplete) {
+      width: 600px;
+      margin-right: 10px;
+    }
+  }
+</style>
