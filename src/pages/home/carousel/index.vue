bd3738be2b60073a6dc87e8993d81ffe95dfc375
fsz0826 <513302151@qq.com>
2025-10-23 10:43:06 +0800

    use el-carousel

---
+++ src/pages/home/carousel/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-carousel height="350px">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <img src="../../../assets/images/web-banner-1.png" alt="轮播图" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss" scoped>
+img {
+  width: 100%;
+  height: 350px;
+}
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>home</div>
+  <div>
+    <Carousel />
+  </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import Carousel from "./carousel/index.vue";
+</script>
 <style lang="scss" scoped></style>
