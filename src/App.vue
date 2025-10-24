5e1ea4c6dc64f0579113ed6502ec2ad212d7f05d
fsz0826 <513302151@qq.com>
2025-10-24 15:41:17 +0800

    测试mock接口

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,25 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { onMounted } from "vue";
+  import request from "@/utils/request";
+  onMounted(() => {
+    request.get("/user?page=3&size=10").then((res) => {
+      console.log(res);
+    });
+  });
+</script>
 
 <style scoped lang="scss">
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  .content {
-    min-height: 700px;
-    width: 1200px;
-    margin-top: 70px;
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .content {
+      min-height: 700px;
+      width: 1200px;
+      margin-top: 70px;
+    }
   }
-}
 </style>
