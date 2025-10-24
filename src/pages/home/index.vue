20f09c6280ca028cd13b0774ce8c3b260107fc85
fsz0826 <513302151@qq.com>
2025-10-24 15:40:04 +0800

    封装分页组件

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -6,6 +6,10 @@
       <el-col :span="20">
         <Level />
         <Region />
+        <div class="hospital">
+          <HospitalCard v-for="item in 10" :key="item" class="item" />
+          <Pagination />
+        </div>
       </el-col>
       <el-col :span="4"> 456 </el-col>
     </el-row>
@@ -16,5 +20,18 @@
   import Search from "./search/index.vue";
   import Level from "./level/index.vue";
   import Region from "./region/index.vue";
+  import HospitalCard from "./card/index.vue";
+  import Pagination from "./pagination/index.vue";
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .hospital {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    .item {
+      width: 48%;
+      margin: 10px 0;
+    }
+  }
+</style>
---
+++ src/pages/home/pagination/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-pagination
+      v-model:current-page="pageNum"
+      v-model:page-size="pageSize"
+      :page-sizes="[10, 20, 30, 40]"
+      :background="true"
+      layout="  prev, pager, next, jumper,sizes,total"
+      :total="13"
+    />
+  </div>
+</template>
+<script lang="ts" setup>
+  import { ref } from "vue";
+
+  const pageNum = ref<number>(1);
+  const pageSize = ref<number>(10);
+</script>
+<style lang="scss" scoped></style>
