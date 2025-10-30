680679ee8f5ec43644f045623c9c0192ac6fbbe5
fsz0826 <513302151@qq.com>
2025-10-30 15:49:18 +0800

    feat: 完成hospital父组件

--- src/pages/hospital/index.vue
+++ src/pages/hospital/index.vue
@@ -1,5 +1,65 @@
 <template>
-  <div>hospital</div>
+  <div class="hospital">
+    <div class="menu">
+      <div class="top">
+        <el-icon><House /></el-icon>
+        <p>/ 医院信息</p>
+      </div>
+      <el-menu default-active="1">
+        <el-menu-item index="1">
+          <el-icon><Menu /></el-icon>
+          <span>预约挂号</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <el-icon><Document /></el-icon>
+          <span>医院详情</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <el-icon><ChatDotSquare /></el-icon>
+          <span>预约通知</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <el-icon><InfoFilled /></el-icon>
+          <span>停诊信息</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <el-icon><Search /></el-icon>
+          <span>查询/取消</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
 </template>
-<script lang="ts" setup></script>
-<style lang="scss" scoped></style>
+<script lang="ts" setup>
+  import {
+    House,
+    Menu,
+    Document,
+    Search,
+    InfoFilled,
+    ChatDotSquare,
+  } from "@element-plus/icons-vue";
+</script>
+<style lang="scss" scoped>
+  .hospital {
+    display: flex;
+    .menu {
+      flex: 2;
+      .top {
+        display: flex;
+        font-size: 14px;
+        color: #7f7f7f;
+        margin-left: 28px;
+        p {
+          margin-left: 6px;
+        }
+      }
+    }
+    .content {
+      flex: 8;
+    }
+  }
+</style>
