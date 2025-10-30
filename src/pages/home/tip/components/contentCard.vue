74c33192dd43e398545620b5752564af21d0b98f
fsz0826 <513302151@qq.com>
2025-10-30 15:38:08 +0800

    使用el-icon

--- src/pages/home/tip/components/contentCard.vue
+++ src/pages/home/tip/components/contentCard.vue
@@ -7,22 +7,7 @@
       </div>
       <div class="right" @click="onMoreClick">
         <span>{{ moreText }}</span>
-        <svg
-          t="1685153789391"
-          class="icon"
-          viewBox="0 0 1024 1024"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          p-id="4663"
-          width="16"
-          height="16"
-        >
-          <path
-            d="M709.845333 250.346667a22.4 22.4 0 0 1 0.533334-30.848 20.48 20.48 0 0 1 29.717333 0.64l272.426667 292.693333-272.426667 292.650667a20.458667 20.458667 0 0 1-29.717333 0.64c-8.32-8.32-8.746667-21.973333-0.533334-30.848l242.346667-262.464-242.346667-262.464z"
-            fill="#3D3D3D"
-            p-id="4664"
-          ></path>
-        </svg>
+        <el-icon><ArrowRight /></el-icon>
       </div>
     </div>
     <div class="content">
@@ -31,6 +16,7 @@
   </div>
 </template>
 <script lang="ts" setup>
+  import { ArrowRight } from "@element-plus/icons-vue";
   interface Props {
     title: string;
     moreText?: string;
@@ -60,9 +46,6 @@
       align-items: center;
     }
     .left {
-      svg {
-        transform: translateY(1px);
-      }
       span {
         margin-left: 5px;
       }
